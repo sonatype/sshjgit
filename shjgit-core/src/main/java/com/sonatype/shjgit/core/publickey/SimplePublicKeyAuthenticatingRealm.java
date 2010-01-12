2acfcba4083d04b03440974a16e285828ba39aae
Hugo Josefson <hugo@josefson.org>
2010-01-12 14:51:39 +0000

    No need for AuthorizingRealm when the Authorizer interface does the job just fine.

--- shjgit-core/src/main/java/com/sonatype/shjgit/core/publickey/SimplePublicKeyAuthenticatingRealm.java
+++ shjgit-core/src/main/java/com/sonatype/shjgit/core/publickey/SimplePublicKeyAuthenticatingRealm.java
@@ -5,9 +5,9 @@ import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationException;
+import org.apache.shiro.authz.Authorizer;
 import org.apache.shiro.authz.Permission;
 import org.apache.shiro.realm.AuthenticatingRealm;
-import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 
 import java.security.PublicKey;
@@ -15,7 +15,8 @@ import java.util.*;
 
 /**
  * Shiro {@link org.apache.shiro.realm.Realm} for authenticating {@link java.security.PublicKey}s.
- * Authorization is delegated to a different {@code Realm}.
+ * Authorization is delegated to an {@link org.apache.shiro.authz.Authorizer},
+ * which can be a different {@code Realm}.
  * This implementation stores accounts internally in memory. For other storage
  * options, it is advisable to subclass this and override {@link #addAccount(Object, java.util.Set)},
  * {@link #hasAccount(Object)} and {@link #getPublicKeysForAccount(Object)}.
@@ -24,17 +25,19 @@ public class SimplePublicKeyAuthenticatingRealm extends AuthenticatingRealm {
 
     private static final Class<PublicKeyAuthenticationToken> AUTHENTICATION_TOKEN_CLASS = PublicKeyAuthenticationToken.class;
     private        final Map<Object, Set<PublicKey>>         accounts                   = new HashMap<Object, Set<PublicKey>>(); //principal-to-publickeys
-    private        final AuthorizingRealm                    authorizingRealm;
+    private        final Authorizer                          authorizer;
 
     /**
-     * Constructs this realm, accepting a realm to which all authorization will be delegated.
+     * Constructs this realm, accepting an {@code Authorizer} to which all
+     * authorization will be delegated.
      *
-     * @param authorizingRealm all authorization will be delegated to this realm.
+     * @param authorizer all authorization will be delegated to this. can be
+     * for example another {@link org.apache.shiro.realm.Realm}.
      */
-    public SimplePublicKeyAuthenticatingRealm(AuthorizingRealm authorizingRealm) {
+    public SimplePublicKeyAuthenticatingRealm(Authorizer authorizer) {
         setAuthenticationTokenClass(AUTHENTICATION_TOKEN_CLASS);
         setCredentialsMatcher(new PublicKeyCredentialsMatcher());
-        this.authorizingRealm = authorizingRealm;
+        this.authorizer = authorizer;
     }
 
     /**
@@ -96,77 +99,77 @@ public class SimplePublicKeyAuthenticatingRealm extends AuthenticatingRealm {
 
     @Override
     public boolean isPermitted(PrincipalCollection principals, String permission) {
-        return authorizingRealm.isPermitted(principals, permission);
+        return authorizer.isPermitted(principals, permission);
     }
 
     @Override
     public boolean isPermitted(PrincipalCollection subjectPrincipal, Permission permission) {
-        return authorizingRealm.isPermitted(subjectPrincipal, permission);
+        return authorizer.isPermitted(subjectPrincipal, permission);
     }
 
     @Override
     public boolean[] isPermitted(PrincipalCollection subjectPrincipal, String... permissions) {
-        return authorizingRealm.isPermitted(subjectPrincipal, permissions);
+        return authorizer.isPermitted(subjectPrincipal, permissions);
     }
 
     @Override
     public boolean[] isPermitted(PrincipalCollection subjectPrincipal, List<Permission> permissions) {
-        return authorizingRealm.isPermitted(subjectPrincipal, permissions);
+        return authorizer.isPermitted(subjectPrincipal, permissions);
     }
 
     @Override
     public boolean isPermittedAll(PrincipalCollection subjectPrincipal, String... permissions) {
-        return authorizingRealm.isPermittedAll(subjectPrincipal, permissions);
+        return authorizer.isPermittedAll(subjectPrincipal, permissions);
     }
 
     @Override
     public boolean isPermittedAll(PrincipalCollection subjectPrincipal, Collection<Permission> permissions) {
-        return authorizingRealm.isPermittedAll(subjectPrincipal, permissions);
+        return authorizer.isPermittedAll(subjectPrincipal, permissions);
     }
 
     @Override
     public void checkPermission(PrincipalCollection subjectPrincipal, String permission) throws AuthorizationException {
-        authorizingRealm.checkPermission(subjectPrincipal, permission);
+        authorizer.checkPermission(subjectPrincipal, permission);
     }
 
     @Override
     public void checkPermission(PrincipalCollection subjectPrincipal, Permission permission) throws AuthorizationException {
-        authorizingRealm.checkPermission(subjectPrincipal, permission);
+        authorizer.checkPermission(subjectPrincipal, permission);
     }
 
     @Override
     public void checkPermissions(PrincipalCollection subjectPrincipal, String... permissions) throws AuthorizationException {
-        authorizingRealm.checkPermissions(subjectPrincipal, permissions);
+        authorizer.checkPermissions(subjectPrincipal, permissions);
     }
 
     @Override
     public void checkPermissions(PrincipalCollection subjectPrincipal, Collection<Permission> permissions) throws AuthorizationException {
-        authorizingRealm.checkPermissions(subjectPrincipal, permissions);
+        authorizer.checkPermissions(subjectPrincipal, permissions);
     }
 
     @Override
     public boolean hasRole(PrincipalCollection subjectPrincipal, String roleIdentifier) {
-        return authorizingRealm.hasRole(subjectPrincipal, roleIdentifier);
+        return authorizer.hasRole(subjectPrincipal, roleIdentifier);
     }
 
     @Override
     public boolean[] hasRoles(PrincipalCollection subjectPrincipal, List<String> roleIdentifiers) {
-        return authorizingRealm.hasRoles(subjectPrincipal, roleIdentifiers);
+        return authorizer.hasRoles(subjectPrincipal, roleIdentifiers);
     }
 
     @Override
     public boolean hasAllRoles(PrincipalCollection subjectPrincipal, Collection<String> roleIdentifiers) {
-        return authorizingRealm.hasAllRoles(subjectPrincipal, roleIdentifiers);
+        return authorizer.hasAllRoles(subjectPrincipal, roleIdentifiers);
     }
 
     @Override
     public void checkRole(PrincipalCollection subjectPrincipal, String roleIdentifier) throws AuthorizationException {
-        authorizingRealm.checkRole(subjectPrincipal, roleIdentifier);
+        authorizer.checkRole(subjectPrincipal, roleIdentifier);
     }
 
     @Override
     public void checkRoles(PrincipalCollection subjectPrincipal, Collection<String> roleIdentifiers) throws AuthorizationException {
-        authorizingRealm.checkRoles(subjectPrincipal, roleIdentifiers);
+        authorizer.checkRoles(subjectPrincipal, roleIdentifiers);
     }
 
 }
