3cd4a621b49727e79d47a837825b037ecc31b3a2
Jason van Zyl <jvanzyl@Fukuyama-2.local>
2010-05-12 21:08:14 -0400

    o separting into normal components

---
+++ sshjgit-core/src/main/java/com/sonatype/sshjgit/core/GitSecurityManager.java
@@ -0,0 +1,100 @@
+package com.sonatype.sshjgit.core;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.apache.shiro.authc.SimpleAccount;
+import org.apache.shiro.authz.Permission;
+import org.apache.shiro.authz.SimpleRole;
+import org.apache.shiro.authz.permission.WildcardPermission;
+import org.apache.shiro.cache.DefaultCacheManager;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.Realm;
+
+import com.sonatype.sshjgit.core.shiro.RolePermissionsAwareSimpleAccountRealm;
+import com.sonatype.sshjgit.core.shiro.publickey.PublicKeyAuthenticatingRealm;
+import com.sonatype.sshjgit.core.shiro.publickey.SimplePublicKeyRepository;
+import com.sonatype.sshjgit.core.util.SshKeyUtils;
+
+@Singleton
+@Named("default")
+public class GitSecurityManager extends DefaultSecurityManager {
+    
+    @Inject
+    public GitSecurityManager( Collection<Realm> realms ) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchProviderException, IOException {
+        
+        super();
+                                
+        String username = System.getProperty("user.name");
+
+        //
+        // this realm can authenticate passwords, and is considered the one which should perform authorization
+        //
+        RolePermissionsAwareSimpleAccountRealm simpleAccountRealm = new RolePermissionsAwareSimpleAccountRealm("simpleAccountRealm");
+        simpleAccountRealm.init();
+        simpleAccountRealm.add(new SimpleRole( "developer", createSampleGroupPermissions() ));
+        simpleAccountRealm.add(new SimpleAccount( username, "test", "simpleAccountRealm", Collections.<String>singleton("developer"), createSampleUserPermissions(username)));
+
+        //
+        // this realm contains allowed public keys for each username. it delegates all authorization to the realm injected in its constructor.
+        //
+        SimplePublicKeyRepository simplePublicKeyRepository = new SimplePublicKeyRepository();
+        simplePublicKeyRepository.addPublicKey(username, loadDefaultPublicKey());
+        PublicKeyAuthenticatingRealm publicKeyRealm = new PublicKeyAuthenticatingRealm(simplePublicKeyRepository, simpleAccountRealm);
+
+        // put both realms in the SecurityManager, so either can authenticate a user
+        setRealms( Arrays.<Realm>asList( simpleAccountRealm, publicKeyRealm ));
+        setCacheManager( new DefaultCacheManager() );
+    }
+    
+    private PublicKey loadDefaultPublicKey() throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchProviderException, IOException {
+        final File file = new File(System.getProperty("user.home") + "/.ssh/id_dsa.pub");
+        return SshKeyUtils.toPublicKey(file);
+    }
+
+    private Set<Permission> createSampleGroupPermissions() {
+        return new HashSet<Permission>() {
+            {
+                // allows normal push to all existing project repos, i.e. /projects/**
+                add(new WildcardPermission("gitrepo:push:projects"));
+
+                // allows fetch (pull) from all existing project repos
+                add(new WildcardPermission("gitrepo:fetch:projects"));
+            }
+        };
+    }
+
+    private Set<Permission> createSampleUserPermissions(final String username) {
+        return new HashSet<Permission>() {
+            {
+                // allows creating new repos under their own directory, i.e. /users/hugo/**
+                add(new WildcardPermission("gitrepo:new:users:"+username));
+
+                // allows permission to non-fast-forward heads in all their own repos
+                add(new WildcardPermission("gitrepo:non-fast-forward:users:"+username));
+
+                // allows normal push to all their own repos
+                add(new WildcardPermission("gitrepo:push:users:"+username));
+
+                // allows fetch (pull) from all their own repos
+                add(new WildcardPermission("gitrepo:fetch:users:"+username));
+
+                // all of these could be simplified as:
+                // add(new WildcardPermission("gitrepo:*:users:"+username));
+            }
+        };
+    }    
+}
---
+++ sshjgit-core/src/main/java/com/sonatype/sshjgit/core/SshdGitServer.java
@@ -0,0 +1,99 @@
+package com.sonatype.sshjgit.core;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.sshd.SshServer;
+import org.apache.sshd.common.NamedFactory;
+import org.apache.sshd.server.UserAuth;
+import org.apache.sshd.server.auth.UserAuthPublicKey;
+import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+
+import com.sonatype.sshjgit.core.gitcommand.GitCommandFactory;
+import com.sonatype.sshjgit.core.shiro.password.ShiroUserAuthPassword;
+import com.sonatype.sshjgit.core.shiro.publickey.ShiroPublickeyAuthenticator;
+import com.sonatype.sshjgit.core.shiro.session.ShiroAwareSshServerSessionFactory;
+
+// Pluggable transport, can I use anything but SSHD right now? SmartHTTPS?
+// Pluggable security manager
+
+@Singleton
+@Named("sshd")
+public class SshdGitServer {
+    
+    @Inject
+    private SecurityManager securityManager;
+    
+    private File repositoriesDirectory;
+    
+    private File configDir;
+    
+    private int port;
+    
+    private SshServer server;
+    
+    @Inject
+    public SshdGitServer( int port, File configDir, File repositoriesDirectory, SecurityManager securityManager ) {
+        this.port = port;
+        this.configDir = configDir;
+        this.repositoriesDirectory = repositoriesDirectory;
+        this.securityManager = securityManager;
+    }
+
+    /**
+     * Constructs a default configured {@link SshServer} for serving up Git repositories.
+     *
+     * @param port which port the ssh server should bind to.
+     * @param reposRootDirectory where the git repositories are stored
+     * @param securityManager the Shiro {@code SecurityManager} which you have preconfigured for authenticating users.
+     * @param hostKeyProvider provider of this server's ssh host keys
+     * @return an {@code SshServer}, ready for you to {@link org.apache.sshd.SshServer#start()}. Please {@link org.apache.sshd.SshServer#stop()} it when it's time for your application to shut down.
+     */
+    public SshServer createDefaultServer() {
+
+        SimpleGeneratorHostKeyProvider hostKeyProvider;
+        
+        if ( configDir == null ){
+            hostKeyProvider =  new SimpleGeneratorHostKeyProvider( null );
+        }else{
+            hostKeyProvider = new SimpleGeneratorHostKeyProvider( configDir + "/sshjgit.hostkeys" );
+        }
+        
+        SshServer server = SshServer.setUpDefaultServer();
+        server.setPort( port );
+        server.setKeyPairProvider( hostKeyProvider );
+        server.setShellFactory( new NoShell() );
+        server.setCommandFactory( new GitCommandFactory( repositoriesDirectory ) );
+        server.setUserAuthFactories( Arrays.<NamedFactory<UserAuth>>asList( new UserAuthPublicKey.Factory(), new ShiroUserAuthPassword.Factory()) );
+        server.setSessionFactory( new ShiroAwareSshServerSessionFactory( securityManager, server ) );
+        server.setPublickeyAuthenticator( new ShiroPublickeyAuthenticator());        
+        return server;
+    }
+    
+    //
+    // Lifecycle
+    //
+    public void start() throws IOException {
+        
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook( new Thread() {
+            @Override
+            public void run() {
+                try {
+                    server.stop();
+                } catch (InterruptedException ignore) {
+                }
+            }
+        } );
+    }
+    
+    public void stop() {        
+    }
+}
